7dca95f0038bc32d318a9abf44619c3e830372cf
Tony Tan <tony.tan@flatiron.com>
2023-07-07 16:16:03 -0500

    Add lambda docker and handler

---
+++ src/lambda.ts
@@ -0,0 +1,18 @@
+import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@app/modules/main/app.module';
+
+let cachedServer;
+
+export const handler = async (event, context) => {
+  if (!cachedServer) {
+    const nestApp = await NestFactory.create(AppModule);
+    await nestApp.init();
+    cachedServer = serverlessExpress({
+      app: nestApp.getHttpAdapter().getInstance(),
+    });
+  }
+
+  return cachedServer(event, context);
+};
+
