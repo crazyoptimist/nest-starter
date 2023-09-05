5c67b0746f129da64dc8dfaf7e1e9227375ee13b
Tony Tan <tony.tan@flatiron.com>
2023-09-05 10:45:16 -0500

    Add ValidationPipe and all other app configs

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,15 +1,27 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { Callback, Context, Handler } from 'aws-lambda';
-import { AppModule } from '@app/modules/main/app.module';
+import { useContainer } from 'class-validator';
 import { ReplaySubject, firstValueFrom } from 'rxjs';
+import { TrimStringsPipe } from '@app/modules/common/transformer/trim-strings.pipe';
+import { setupSwagger } from '@app/swagger';
+import { AppModule } from '@app/modules/main/app.module';
+
 
 //Declare a ReplaySubject to store the serverlessExpress instance.
 const serverSubject = new ReplaySubject<Handler>()
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  setupSwagger(app);
+  app.enableCors();
+  app.useGlobalPipes(
+    new TrimStringsPipe(),
+    new ValidationPipe({ whitelist: true }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.init();
   const expressApp = app.getHttpAdapter().getInstance();
